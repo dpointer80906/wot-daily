2d3e8dd18578d45d6f58eae0e28431cdd572f95a
David Pointer <dpointer@directstream.com>
2017-04-03 16:06:39 -0600

    Initial account tank id to tank name mapping.

--- DailyStats.py
+++ DailyStats.py
@@ -19,9 +19,18 @@ class DailyStats(object):
         '''int: '''
         return self._account_id
 
+    @property
+    def vehicles(self):
+        '''dict: tank_id: tank_name'''
+        return self._vehicles
+
     def __init__(self, args):
         self._wot = wargaming.WoT(args.appid, region='na', language='en')
         player = self.wot.account.list(search=args.player)
         self._account_id = player[0]['account_id']
-        result = self.wot.account.tanks(account_id=self.account_id)
-        pprint.pprint(result[self.account_id])
+        account_vehicles = self.wot.account.tanks(account_id=self.account_id, fields="tank_id")
+        account_tank_ids = [x['tank_id'] for x in account_vehicles[self.account_id]]
+        vehicle_id2names = self.wot.encyclopedia.vehicles(fields='name')
+        self._vehicles = {tank_id: vehicle_id2names[tank_id]['name'] for tank_id in vehicle_id2names.keys()}
+        for tid in account_tank_ids:
+            print self.vehicles[str(tid)]
